121ba168b1aa57ad638c5adcdccfaec5fcbe0b82
François Delbrayelle <fdelbrayelle@gmail.com>
2025-09-10 10:52:42 +0200

    fix(): add missing category to TRANSFORMATION

---
+++ plugin-transform-grok/src/main/java/io/kestra/plugin/transform/grok/package-info.java
@@ -0,0 +1,4 @@
+@PluginSubGroup(title = "Grok", categories = PluginSubGroup.PluginCategory.TRANSFORMATION)
+package io.kestra.plugin.transform.grok;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
---
+++ plugin-transform-json/src/main/java/io/kestra/plugin/transform/jsonata/package-info.java
@@ -0,0 +1,4 @@
+@PluginSubGroup(title = "JSONata", categories = PluginSubGroup.PluginCategory.TRANSFORMATION)
+package io.kestra.plugin.transform.jsonata;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
