fda925b007b9aa4ce90632df6cfffdaf1c06eaec
Georg Traar <proddata@users.noreply.github.com>
2026-03-16 09:48:43 +0100

    feat(transform): add records transform tasks (#54)
    * feat(transform): add records transform module migration
    
    ---------
    
    Co-authored-by: François Delbrayelle <fdelbrayelle@kestra.io>

---
+++ plugin-transform-records/src/main/java/io/kestra/plugin/transform/expression/ExpressionException.java
@@ -0,0 +1,11 @@
+package io.kestra.plugin.transform.expression;
+
+public class ExpressionException extends Exception {
+    public ExpressionException(String message) {
+        super(message);
+    }
+
+    public ExpressionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
