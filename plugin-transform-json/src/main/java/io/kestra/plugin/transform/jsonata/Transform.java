b1ddee1a6c344f8f1733393726238730a191285c
François Delbrayelle <fdelbrayelle@kestra.io>
2026-03-25 18:16:24 +0100

    Replace JSONata4Java with dashjoin/jsonata-java for full JSONata compatibility
    JSONata4Java is not a complete implementation of the JSONata spec, causing
    expressions that work on try.jsonata.org to fail in Kestra (e.g. nested array
    path expressions). Switch to dashjoin/jsonata-java which is a 1:1 port of the
    JavaScript reference implementation with 100% feature parity.
    
    Fixes #40

--- plugin-transform-json/src/main/java/io/kestra/plugin/transform/jsonata/Transform.java
+++ plugin-transform-json/src/main/java/io/kestra/plugin/transform/jsonata/Transform.java
@@ -1,21 +1,24 @@
 package io.kestra.plugin.transform.jsonata;
 
-import com.api.jsonata4java.expressions.EvaluateException;
-import com.api.jsonata4java.expressions.Expressions;
-import com.api.jsonata4java.expressions.ParseException;
+import static com.dashjoin.jsonata.Jsonata.jsonata;
+
+import com.dashjoin.jsonata.JException;
+import com.dashjoin.jsonata.Jsonata;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.NullNode;
 import io.kestra.core.exceptions.IllegalVariableEvaluationException;
 import io.kestra.core.models.property.Property;
 import io.kestra.core.models.tasks.Output;
 import io.kestra.core.models.tasks.RunnableTask;
 import io.kestra.core.models.tasks.Task;
 import io.kestra.core.runners.RunContext;
+import io.kestra.core.serializers.JacksonMapper;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
 
-import java.io.IOException;
 import java.time.Duration;
 
 import io.kestra.core.models.enums.MonacoLanguages;
@@ -28,6 +31,8 @@ import io.kestra.core.models.annotations.PluginProperty;
 @NoArgsConstructor
 public abstract class Transform<T extends Output> extends Task implements JSONataInterface, RunnableTask<T> {
 
+    private static final ObjectMapper MAPPER = JacksonMapper.ofJson();
+
     @PluginProperty(language = MonacoLanguages.JAVASCRIPT)
     private Property<String> expression;
 
@@ -35,29 +40,35 @@ public abstract class Transform<T extends Output> extends Task implements JSONat
     private Property<Integer> maxDepth = Property.ofValue(1000);
 
     @Getter(AccessLevel.PRIVATE)
-    private Expressions expressions;
+    private Jsonata parsedExpression;
 
     public void init(RunContext runContext) throws Exception {
-        this.expressions = parseExpression(runContext);
+        var exprString = runContext.render(this.expression).as(String.class).orElseThrow();
+        try {
+            this.parsedExpression = jsonata(exprString);
+        } catch (JException e) {
+            throw new IllegalArgumentException("Invalid JSONata expression. Error: " + e.getMessage(), e);
+        }
     }
 
     protected JsonNode evaluateExpression(RunContext runContext, JsonNode jsonNode) {
         try {
-            long timeoutInMilli = runContext.render(getTimeout()).as(Duration.class)
+            var timeoutInMilli = runContext.render(getTimeout()).as(Duration.class)
                 .map(Duration::toMillis)
                 .orElse(Long.MAX_VALUE);
+            var rMaxDepth = runContext.render(getMaxDepth()).as(Integer.class).orElseThrow();
 
-            return this.expressions.evaluate(jsonNode, timeoutInMilli, runContext.render(getMaxDepth()).as(Integer.class).orElseThrow());
-        } catch (EvaluateException | IllegalVariableEvaluationException e) {
+            var data = MAPPER.convertValue(jsonNode, Object.class);
+            var frame = this.parsedExpression.createFrame();
+            frame.setRuntimeBounds(timeoutInMilli, rMaxDepth);
+
+            var result = this.parsedExpression.evaluate(data, frame);
+            if (result == null) {
+                return NullNode.getInstance();
+            }
+            return MAPPER.valueToTree(result);
+        } catch (JException | IllegalVariableEvaluationException e) {
             throw new RuntimeException("Failed to evaluate expression", e);
-        }
-    }
-
-    private Expressions parseExpression(RunContext runContext) throws IllegalVariableEvaluationException {
-        try {
-            return Expressions.parse(runContext.render(this.expression).as(String.class).orElseThrow());
-        } catch (ParseException | IOException e) {
-            throw new IllegalArgumentException("Invalid JSONata expression. Error: " + e.getMessage(), e);
         }
     }
 }
